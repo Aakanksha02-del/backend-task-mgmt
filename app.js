5a96d08ca6bc8bdf8aacb8d93f9aec6eaf618205
aakanksha <aakankshasolanki2004@gmail.com>
2026-04-09 16:23:56 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const path =require('path')
+
+const dbConn = require('./config/db')
+const association = require('./models/associations')
+const taskRouter = require('./routes/taskRoute')
+const userRouter = require('./routes/userRoute')
+const assignTaskRouter = require('./routes/assignTaskRoute')
+
+
+
+const port = process.env.PORT 
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/',(req,res)=>{
+    res.send("I am Server ")
+})
+
+app.use('/tasks', taskRouter)
+// create route user 
+app.use('/user',userRouter)
+app.use('/assignTask', assignTaskRouter)
+
+// app.get('/upload', express.static('upload'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+
+app.listen(port,()=>{
+    console.log(`server running on http://localhost:${port}`)
+})
